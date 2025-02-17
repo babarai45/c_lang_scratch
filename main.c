1b2074cf608ab82d2b54eb61918e32d09fc2bce8
babarai45 <muhammadbabarai45@gmail.com>
2025-02-17 12:41:02 +0500

    test

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-  printf("Hello World!\n");
+  printf("Enter your name: ");
+  char name[100];
+  scanf("%s", name);
+  printf("Enter your age: ");
+  int age;
+  scanf("%d", &age);
+  printf("Your name is: %s\n", name);
+  printf("Your age is: %d\n", age);
   return 0;
+
 }
